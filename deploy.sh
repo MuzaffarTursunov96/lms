5ecb7b87c52ab5d133dd5becf524afb291091df0
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2025-09-20 17:43:06 +0500

    corected

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git pull origin main
+docker-compose down
+docker-compose up -d --build
+docker-compose exec web_lms python manage.py migrate --noinput
+docker-compose exec web_lms python manage.py collectstatic --noinput
