fc84ec9f5d2b74397157210dcd839d59672be783
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2025-10-01 11:32:12 +0500

    corected

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-with open("data.json", "r", encoding="utf-8-sig") as f:
-    data = f.read()
+nums =[1,2,3]
 
-with open("data_clean.json", "w", encoding="utf-8") as f:
-    f.write(data)
+
+    
+class Solution(object):
+    def permute(self, nums):
+        result = []
+        n = len(nums)
+        counter = 0
+        
+        while counter <= n:
+            
+            for i in range(counter+1, n):
+                new_nums = nums.copy()
+                new_nums[counter], new_nums[i] = new_nums[i], new_nums[counter]
+                print(nums)
+                result.append(new_nums)
+            counter += 1
+        
+        return result
+    
+a = Solution()
+print(a.permute(nums))
+
+
